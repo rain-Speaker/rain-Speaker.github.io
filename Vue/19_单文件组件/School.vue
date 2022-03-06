d666e427facce81127e7dc44f161725045bfcbb8
rain-Speaker <77324283+rain-Speaker@users.noreply.github.com>
2022-03-06 16:15:18 +0800

    Add files via upload

---
+++ Vue/19_单文件组件/School.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="school">
+    <h3>学校名称：{{ name }}</h3>
+    <h3>学校地址：{{ address }}</h3>
+    <button @click="showName">点击显示学校名称</button>
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码（数据、方法等）
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "Vue",
+      address: "北京",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+<style>
+/* 组件的样式 */
+.school {
+  background-color: orange;
+}
+</style>
---
+++ Vue/19_单文件组件/Student.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 组件的结构 -->
+  <div>
+    <h3>学生姓名：{{ name }}</h3>
+    <h3>学校年龄：{{ age }}</h3>
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码（数据、方法等）
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "Tom",
+      age: 18,
+    };
+  },
+};
+</script>
+
+<style>
+/* 组件的样式 */
+</style>
